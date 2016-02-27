54cf02b6a5d8312d24a99a083b1babe0fbdec336
lanltbl <lanchangqing@ltbl.cn>
2016-02-27 15:14:40 +0800

    添加python的冒泡排序

---
+++ python/1.py
@@ -0,0 +1,19 @@
+#coding:utf-8
+'''
+冒泡排序python实现
+'''
+class Solution(object):
+    def bubble_sort(self, L1):
+        l = len(L1)
+        for i in range(l):
+            for j in range(i, l):
+                if L1[i] > L1[j]:
+                    L1[i], L1[j] = L1[j], L1[i]
+        return L1
+
+a = [6, 2, 4, 1, 5, 9]
+
+S = Solution()
+b = S.bubble_sort(a)
+print b
+assert [1, 2, 4, 5, 6, 9] == b
